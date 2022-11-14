de989692d91e6f6ab6262b777ab832924b14e1bd
Saqib Iqbal <102893067+saqibiqball@users.noreply.github.com>
2022-11-14 20:16:25 +0500

    revert next.config.js due to build failure

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
-// module.exports = {
-// 	basePath: '',
-// 	reactStrictMode: true,
-// 	images: {
-// 		domains: [
-// 			'localhost',
-// 			'stage.wprollers.com',
-// 			'crowdfunding.dev.wprollers.com',
-// 			'crowdfunding-app.dev.wprollers.com',
-// 		],
-// 		formats: ['image/webp'],
-// 	},
-// 	eslint: {
-// 		// Warning: This allows production builds to successfully complete even if
-// 		// your project has ESLint errors.
-// 		ignoreDuringBuilds: true,
-// 	},
-// };
+module.exports = {
+	basePath: '',
+	reactStrictMode: true,
+	images: {
+		domains: [
+			'localhost',
+			'stage.wprollers.com',
+			'crowdfunding.dev.wprollers.com',
+			'crowdfunding-app.dev.wprollers.com',
+		],
+		formats: ['image/webp'],
+	},
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
+};
